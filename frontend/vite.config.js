16e122bf3aa2195a928d5cb27add3f1ac8bcdef5
evanmrosas <147665124+evanmrosas@users.noreply.github.com>
2025-06-20 09:08:53 -0500

    Allow Render host in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Allow the Render URL
+    allowedHosts: ['class-management-frontend.onrender.com'],
+    host: true // This is also helpful for Docker
+  }
 })
